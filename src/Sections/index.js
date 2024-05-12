4e334b0a98e5a3f861aa92ea3f4ee22fb2ab46c9
Ankit <at0633750@gmail.com>
2024-05-12 23:14:12 +0530

    first day

---
+++ src/Sections/index.js
@@ -0,0 +1,18 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import SpecialOffer from "./SpecialOffer";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+export{
+    Hero,
+    PopularProducts,
+    Footer,
+    CustomerReviews,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+}
