78423f51e42ce079b54ae77060dbd27bbb8bbcc7
Ankit <at0633750@gmail.com>
2024-05-14 23:53:18 +0530

    upto services section

--- src/Sections/SuperQuality.jsx
+++ src/Sections/SuperQuality.jsx
@@ -1,4 +1,5 @@
 import Button from "../Components/Button"
+import { shoe8 } from "../assets/images"
 
 const SuperQuality = () => {
   return (
@@ -8,19 +9,27 @@ const SuperQuality = () => {
        
 
         <h2 className=' font-palanquin text-4xl capitalize font-bold lg:max-w-lg'>
-          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
-            The New Arrival
-          </span>
-          <br />
-          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
+         We Provide You
+          <span className='text-coral-red'>Super </span> 
+          <span className='text-coral-red'>Quality</span> Shoes
         </h2>
-        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
-          Discover stylish Nike arrivals, quality comfort, and innovation for
-          your active life.
+        <p className='mt-4 lg:max-w-lg info-text '>
+        Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.
         </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+        Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11">
 
         <Button label='View details' />
+        </div>
       
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img src={shoe8} alt="shoe8"
+        width={570}
+        height={522} 
+        className="object-contain"/>
       </div>
 
     </section>
