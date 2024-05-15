2904df6b806dfdf82fbacb543f34b1727bc926bc
Ankit <at0633750@gmail.com>
2024-05-15 22:33:30 +0530

    final day

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,9 +1,9 @@
 
-const Button = ({label, iconURL, backgroundColor, textColor, borderColor}) => {
+const Button = ({label, iconURL, backgroundColor, textColor, borderColor, fullWidth}) => {
   return (
-    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-non
+    <button className={`flex justify-center rounded-full items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-non
     ${backgroundColor ?  `${backgroundColor} ${textColor} ${borderColor}`:
-  "bg-coral-red  text-white border-coral-red"}rounded-full w-full"}`}>
+  "bg-coral-red  text-white border-coral-red"}  ${fullWidth && `w-full`}`}>
 
     {label}
     {iconURL &&
